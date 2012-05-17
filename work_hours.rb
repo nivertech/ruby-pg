14c10abf90d0420131e610d6a2ca35b6d8d8e3d9
Ori Bar <ori.bar@nivertech.com>
2012-05-17 14:18:28 +0200

    sum work hours

---
+++ work_hours.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/ruby
+(`xls2csv "#{ENV['HOME']}/Dropbox/OriB/timesheet/may2012.xls"`. # get data from worksheet excel
+ map {|x| x.split(",")[3] }.                                    # filter out work hours (measured in days)
+ find_all {|x| x}.                                              # get rid of nulls
+ map { |x| x[1..-1].to_f * 24.0 }.                              # convert days to hours
+ inject {|sum, n| sum + n})                                     # summarize results
